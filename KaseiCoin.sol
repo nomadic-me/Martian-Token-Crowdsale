eb673208ad6a2f3b8cfa16c8a694caee4fbd2e0f
Satish Surath <s@n-m.co>
2022-07-16 17:41:38 -0400

    adding KaseiCoin smart contract code and inheriting the ERC20 OpenZeppelin libraries
    Signed-off-by: Satish Surath <s@n-m.co>

--- KaseiCoin.sol
+++ KaseiCoin.sol
@@ -9,3 +9,21 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
 // Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+
+
+contract KaseiCoin is ERC20, ERC20Detailed, ERC20Mintable {
+    address payable owner;
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You do not have permission to mint these tokens!");
+        _;
+    }
+
+    constructor(uint initial_supply) ERC20Detailed("KaseiCoin", "KSC", 18) public {
+        owner = msg.sender;
+        _mint(owner, initial_supply);
+    }
+
+    function mint(address recipient, uint amount) public onlyOwner {
+        _mint(recipient, amount);
+    }
